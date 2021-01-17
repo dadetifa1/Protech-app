c1da2b698bb10d84e781eaaaae53728a7fca69d7
Dele <dadetifa1@yahoo.com>
2021-01-17 09:02:18 -0600

    added in some smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
+import SalesEntry from './SalesEntry/SaleEntry';
+import Landing from './Landing/Landing';
+import SalesEntryUpdate from './SalesEntryUpdate/SaleEntryUpdate';
+import SalesCollection from './SalesCollection/SalesCollection';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders the App without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    div,
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders SalesEntery without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<SalesEntry />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Landing without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <Router>
+      <Landing />
+    </Router>,
+    div,
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders SalesEntryUpdate without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<SalesEntryUpdate />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders SalesCollection without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<SalesCollection />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
 });
